fe55bd8170621831537f81262bb90bb7e3e60c9c
Mo <rxcod3r731@gmail.com>
2024-03-26 21:07:46 -0400

    Updated else statement in index.js

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -15,11 +15,9 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1) {
+} else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins!";
-}
-else (randomNumber1 === randomNumber2) {
+} else {
   document.querySelector("h1").innerHTML = "Draw!";
 }
 
