f1286ff5007fcb22e931442186e0b1d1baad1762
Mo <rxcod3r731@gmail.com>
2024-03-27 15:27:23 -0400

    complete drum kit

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -2,11 +2,77 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    this.style.color = "white";
+    //this.style.color = "white";
 
     var buttonInnerHTML = this.innerHTML; //this letter is clicked
+    makeSound(buttonInnerHTML);
 
-    switch (buttonInnerHTML) {
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+//detecting keyboard press
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+});
+
+//var audio = new Audio("sounds/tom-1.mp3");
+//   audio.play();
+//this.style.color = "white";
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "a":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play(); //calling the play function
+      break;
+
+    case "s":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "l":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
+
+/*switch (buttonInnerHTML) {
       case "w":
         var tom4 = new Audio("sounds/tom-4.mp3");
         tom4.play();
@@ -14,7 +80,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
       case "a":
         var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
+        tom3.play(); //calling the play function
         break;
 
       case "s":
@@ -44,10 +110,4 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
       default:
         console.log(buttonInnerHTML);
-    }
-  });
-}
-
-//var audio = new Audio("sounds/tom-1.mp3");
-//   audio.play();
-//this.style.color = "white";
+    } */
