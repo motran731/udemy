4738df1df2dfec58cb3c0689a92103519229ede7
Mo <rxcod3r731@gmail.com>
2024-04-19 15:40:05 -0400

    Used jQuery to detect when buttons clicked

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,9 +1,23 @@
-let gamePattern = [];
-
 let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+});
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
