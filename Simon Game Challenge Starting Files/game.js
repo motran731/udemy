afdc1629191beb0c7ba29833ee700e8042a10898
Mo <rxcod3r731@gmail.com>
2024-04-19 14:22:50 -0400

    Added game.js and created nextSequence function

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,9 @@
+let gamePattern = [];
+
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+}
