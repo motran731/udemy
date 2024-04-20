8ed61931f48b9a7732089b1ee93bf19f74c3a4e2
Mo <rxcod3r731@gmail.com>
2024-04-20 15:09:47 -0400

    Added animations to userclicks and start the game

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,13 +1,27 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level" + level);
+    nextSequence();
+    started = true;
+  }
+});
 
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+  //console.log(userChosenColour)
+  playSound(userChosenColour);
 });
 
 function nextSequence() {
+  level++;
+
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -18,6 +32,25 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  // var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  // audio.play();
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+// Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
+
+// 4. use Google/Stackoverflow to figure out how you can use Javascript to remove the pressed class after a 100 milliseconds.
+
+// Once complete, you will get this effect when you click on any button.
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
