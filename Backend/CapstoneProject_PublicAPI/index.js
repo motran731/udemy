3a8fde754910136fd3c501bbbeaacd67d8bfa09c
Mo <rxcod3r731@gmail.com>
2024-04-30 15:47:52 -0400

    Changed the CSS for capstone API project

--- Backend/CapstoneProject_PublicAPI/index.js
+++ Backend/CapstoneProject_PublicAPI/index.js
@@ -1,8 +1,11 @@
-// HINTS:
-// 1. Import express and axios
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
 
-// 2. Create an express app and set the port number.
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 // 3. Use the public folder for static files.
 
 // 4. When the user goes to the home page it should render the index.ejs file.
@@ -10,4 +13,10 @@
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
 
-// 6. Listen on your predefined port and start the server.
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`You got served on port ${port}!!`);
+});
