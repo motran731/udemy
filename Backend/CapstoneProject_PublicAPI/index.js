91cdbd3ba4855448cd4b4519b879d716e2c7fb9b
Mo <rxcod3r731@gmail.com>
2024-04-30 20:35:20 -0400

    Added d&d classes

--- Backend/CapstoneProject_PublicAPI/index.js
+++ Backend/CapstoneProject_PublicAPI/index.js
@@ -6,15 +6,32 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// 3. Use the public folder for static files.
 
 // 4. When the user goes to the home page it should render the index.ejs file.
 
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+// app.get("/", (req, res) => {
+//   res.render("index.ejs");
+// });
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://www.dnd5eapi.co/api/classes");
+    const data = response.data;
+    //console.log(data);
+
+    const classesArray = data.results;
+    const randomClasses =
+      classesArray[Math.floor(Math.random() * classesArray.length)];
+    console.log(randomClasses);
+
+    res.render("index.ejs", { data: data });
+  } catch (error) {
+    console.error(error.response.data);
+    res.status(500);
+  }
 });
 
 app.listen(port, () => {
