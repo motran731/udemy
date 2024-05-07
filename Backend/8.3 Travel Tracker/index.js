dd007638df165ce73a83a90b6bc7bca2f04924cf
Midori <midori@Midoris-MacBook-Pro.local>
2024-05-07 15:55:04 -0400

    Insert new country using postgre

--- Backend/8.3 Travel Tracker/index.js
+++ Backend/8.3 Travel Tracker/index.js
@@ -29,6 +29,20 @@ app.get("/", async (req, res) => {
   db.end();
 });
 
+//insert new country
+app.post("/add", async (req, res) => {
+  const input =req.body["country"]
+  const result= await db.query("SELECT country_code FROM countries WHERE country_name=$1",[input])
+
+
+  await db.query(
+    "INSERT INTO visited_countries (country_code)VALUES($1)"
+  );
+
+
+
+  const result = await db.query("UPDATE visited_countries
+});
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
