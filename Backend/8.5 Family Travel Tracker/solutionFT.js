cb7e3f5166e239ef5c0e18a5cc530db1d276cb3e
Midori <midori@Midoris-MacBook-Pro.local>
2024-05-10 15:47:54 -0400

    Edited the index.ejs file, if countries not on the map it will console.log unavailable country

--- Backend/8.5 Family Travel Tracker/solutionFT.js
+++ Backend/8.5 Family Travel Tracker/solutionFT.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "postgres",
+  password: "123456",
   port: 5432,
 });
 db.connect();
@@ -20,18 +20,17 @@ app.use(express.static("public"));
 let currentUserId = 1;
 
 let users = [
-  { id: 1, name: "Mo", color: "purple" },
-  { id: 2, name: "Alex", color: "pink" },
+  { id: 1, name: "Angela", color: "teal" },
+  { id: 2, name: "Jack", color: "powderblue" },
 ];
 
-//users.id(PK), user_id(FK)
+//adding total countries but not changing colors
 async function checkVisited() {
   const result = await db.query(
-    "SELECT country_code FROM visited_countries JOIN users ON users.id=user_id WHERE user_id=$1;",
+    "SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1; ",
     [currentUserId]
   );
   let countries = [];
-  console.log(result.rows);
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
@@ -39,14 +38,16 @@ async function checkVisited() {
 }
 
 async function getCurrentUser() {
-  const result = await db.query("SELECT * from users");
+  const result = await db.query("SELECT * FROM users");
   users = result.rows;
-  console.log(users);
   return users.find((user) => user.id == currentUserId);
+  console.log(typeof user.id);
+  console.log(typeof currentUserId);
 }
 
 app.get("/", async (req, res) => {
   const countries = await checkVisited();
+  console.warn(countries);
   const currentUser = await getCurrentUser();
   res.render("index.ejs", {
     countries: countries,
@@ -55,11 +56,10 @@ app.get("/", async (req, res) => {
     color: currentUser.color,
   });
 });
-
-//adding country
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
-  const currentUserId = await getCurrentUser();
+  const currentUser = await getCurrentUser();
+
   try {
     const result = await db.query(
       "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
@@ -70,7 +70,7 @@ app.post("/add", async (req, res) => {
     const countryCode = data.country_code;
     try {
       await db.query(
-        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2) RETURNING*",
         [countryCode, currentUserId]
       );
       res.redirect("/");
@@ -79,14 +79,9 @@ app.post("/add", async (req, res) => {
     }
   } catch (err) {
     console.log(err);
-    const countries = await checkVisited();
-    res.render("index.ejs", {
-      countries: countries,
-      total: countries.length,
-      error: "Country name does not exist, try again",
-    });
   }
 });
+
 app.post("/user", async (req, res) => {
   if (req.body.add === "new") {
     res.render("new.ejs");
@@ -97,16 +92,17 @@ app.post("/user", async (req, res) => {
 });
 
 app.post("/new", async (req, res) => {
-  //Hint: The RETURNING keyword can return the data that was inserted.
-  //https://www.postgresql.org/docs/current/dml-returning.html
-  const name = req.body["name"];
-  const color = req.body["color"];
-  await db.query(
-    "INSERT INTO users (id, name,color) VALUES ($1 , $2)RETURNING *;",
+  const name = req.body.name;
+  const color = req.body.color;
+
+  const result = await db.query(
+    "INSERT INTO users (name, color) VALUES($1, $2) RETURNING *;",
     [name, color]
   );
+
   const id = result.rows[0].id;
   currentUserId = id;
+
   res.redirect("/");
 });
 
