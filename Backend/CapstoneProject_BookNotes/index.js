9a8ef8d01a7915d77e364eb23b9c0eb97b1a731c
Midori <midori@Midoris-MacBook-Pro.local>
2024-05-14 15:15:59 -0400

    Added a post request to add books

---
+++ Backend/CapstoneProject_BookNotes/index.js
@@ -0,0 +1,55 @@
+import express, { query } from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "books",
+  password: "postgres",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let books = [];
+
+app.get("/", async (req, res) => {
+  try {
+    //const image = "https://covers.openlibrary.org/b/isbn/${isbn}-M.jpg";
+    const result = await db.query("SELECT * FROM books ORDER BY id ASC");
+    books = result.rows;
+    res.render("index.ejs", {
+      listTitle: "Books List",
+      listBooks: books,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/add", async (req, res) => {
+  const title = req.body.title;
+  const author = req.body.author;
+  const isbn = req.body.isbn;
+
+  try {
+    await db.query(
+      "INSERT INTO books (title,author,isbn) VALUES ($1, $2, $3)",
+      [title, author, isbn]
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`You got served on http://localhost:${port}`);
+});
