5171af0d879521b78cb1b40155caa879428ffcf1
Midori <midori@Midoris-MacBook-Pro.local>
2024-06-11 21:34:51 -0400

    New notes added

--- React/Keeper-app-part-3-starting/src/components/App.jsx
+++ React/Keeper-app-part-3-starting/src/components/App.jsx
@@ -7,9 +7,9 @@ import CreateArea from "./CreateArea";
 function App() {
   const [notes, setNotes] = useState([]);
 
-  function addNote(inputNote) {
+  function addNote(newNote) {
     setNotes((prevNotes) => {
-      return [...prevNotes, inputNote];
+      return [...prevNotes, newNote];
     });
     //setInputText("");
   }
@@ -17,9 +17,11 @@ function App() {
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      {notes.map((addNotes, index) => (
-        <Note key={1} title="Note title" content="Note content" />
-      ))}
+      {notes.map((noteItem, index) => {
+        return <Note title={noteItem.title} content={noteItem.content} />;
+      })}
+
+      <Note key={1} title="Note title" content="Note content" />
 
       <Footer />
     </div>
