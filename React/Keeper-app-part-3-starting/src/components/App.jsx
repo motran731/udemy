8643f8eb9e26ac77df4c1cb0c950902e682c638c
Midori <midori@Midoris-MacBook-Pro.local>
2024-06-11 21:49:30 -0400

    Delete button can delete notes

--- React/Keeper-app-part-3-starting/src/components/App.jsx
+++ React/Keeper-app-part-3-starting/src/components/App.jsx
@@ -13,15 +13,29 @@ function App() {
     });
     //setInputText("");
   }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
       {notes.map((noteItem, index) => {
-        return <Note title={noteItem.title} content={noteItem.content} />;
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
       })}
-
-      <Note key={1} title="Note title" content="Note content" />
 
       <Footer />
     </div>
